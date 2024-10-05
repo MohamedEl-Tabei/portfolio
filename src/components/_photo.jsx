4283469262fc5ad5d4fc0de394e46f5f377a50e2
unknown <mohamedeltab33y@gmail.com>
2024-10-05 18:39:59 +0300

    first commit

--- src/components/_photo.jsx
+++ src/components/_photo.jsx
@@ -2,7 +2,7 @@ const Photo=()=>{
     const link="https://i.ibb.co/DWvVXPN/image.png"
     return(
         <div className="photo ">
-            <img className="" src={link} style={{width:670,maxHeight:"100%"}} alt="Mohamed Eltabei"  />
+            <img className="" src={link} style={{height:"100%"}} alt="Mohamed Eltabei"  />
         </div>
     )
 }
